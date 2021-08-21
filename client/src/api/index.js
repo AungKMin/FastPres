bf21881a643e430115293cfb25b51ff7e8f06d74
Aung Khant Min <aungkhantmin2014@gmail.com>
2021-08-21 10:12:05 -0400

    change title text and image

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,6 +2,4 @@ import axios from 'axios';
 
 const API = axios.create();
 
-export const test = (newPost) => axios.post('http://localhost:8000/pptx/', newPost, {responseType: 'blob'});
-
-// https://fastpres.herokuapp.com/
+export const test = (newPost) => axios.post('tp://localhost:8000/pptx/', newPost, {responseType: 'blob'});
